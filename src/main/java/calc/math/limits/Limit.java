a11ab7db828036c7eb0d3500655b13ae3e1d577b
Sergey F <chukak12@gmail.com>
2021-01-06 01:32:44 +0300

    Using a numerical approximation of a limit.
    Information:
    Now limits calculating using a numerical approximation method.
    Calculating limits from below and above.
    Tests.
    The 'Limit' class has public members that can be changed by developers.

--- src/main/java/calc/math/limits/Limit.java
+++ src/main/java/calc/math/limits/Limit.java
@@ -12,10 +12,30 @@ class Limit
 {
 
 /**
+ * Coefficient, which used in approximation to limit.
+ */
+public static double EPSILON_COEFFICIENT = 0.000000001;
+/**
+ * Precision of approximation to limit.
+ */
+public static double APPROXIMATION_PRECISION = 10;
+
+/**
+ * Math limits types.
+ */
+private
+enum LimitType
+{
+	BELOW,
+	ABOVE
+}
+
+/**
  * Parses a math limit expression and calculates it.
  * <p>
  * In simple math limits just substitute a value instead of variable using the variable name. After
- * this calculate a result of updated expression.
+ * this calculate a result of updated expression. To calculating limit using a numerical
+ * approximation of a limit. But, at the moment not all math limits calculated correctly.
  *
  * @param source a math limit expression.
  * @param variableName variable name (x, n, etc).
@@ -25,15 +45,67 @@ class Limit
  * throws exception.
  */
 public static
-double calculate(String source, String variableName, double approachTo) throws Exception
+double calculate(String source, String variableName, double approachTo, double epsilon) throws
+																																												Exception
 {
-	String copySource = source.replaceAll(variableName, Double.toString(approachTo));
-	double result = Expression.calculate(copySource);
-	if(Double.isNaN(result)) {
-		// todo: analyze source string and avoid divide by zero
-		// todo: l'hopital's rule and other
-		return Double.NaN;
+	double limitFromBelow = getLimitFrom(LimitType.BELOW, source, variableName, approachTo);
+	double limitFromAbove = getLimitFrom(LimitType.ABOVE, source, variableName, approachTo);
+	if(equals(Math.abs(limitFromBelow), Math.abs(limitFromAbove), epsilon)) { return limitFromAbove; }
+	return Double.NaN;
+}
+
+/**
+ * Calculates limit from above or below, using the 'Expression' class.
+ *
+ * @param type limit type.
+ * @param source a math expression.
+ * @param variableName variable name (x, n, etc).
+ * @param approachTo limit strives for.
+ * @return result of calculation this math limit.
+ * @throws Exception if an error occurred while parsing this expression or 'Expression.calculate'
+ * throws exception.
+ */
+private static
+double getLimitFrom(
+		LimitType type, String source, String variableName, double approachTo) throws Exception
+{
+	int precision = 0;
+	for(double d = approachTo; precision < APPROXIMATION_PRECISION;
+		/* increment precision */ ++precision) {
+		String copySource = source.replaceAll(variableName, Double.toString(d));
+		double result = Expression.calculate(copySource);
+
+		if(!Double.isNaN(result)) {
+			return result;
+		}
+
+		// todo: limits approach to infinity
+		// todo: perfect (?) limit
+		// todo: 1 ^ infinity
+
+		switch(type) {
+		case ABOVE:
+			d = d + EPSILON_COEFFICIENT;
+			break;
+		case BELOW:
+			d = d - EPSILON_COEFFICIENT;
+			break;
+		}
 	}
-	return result;
+	return Double.NaN;
+}
+
+/**
+ * Compares two math limits.
+ *
+ * @param limitFromBelow limit from below, not NaN.
+ * @param limitFromAbove limit from above, not NaN.
+ * @param epsilon epsilon.
+ * @return result of comparison two limits.
+ */
+private static
+boolean equals(double limitFromBelow, double limitFromAbove, double epsilon)
+{
+	return limitFromAbove == limitFromBelow || Math.abs(limitFromBelow - limitFromAbove) < epsilon;
 }
 }
--- src/test/java/calc/math/limits/LimitTest.java
+++ src/test/java/calc/math/limits/LimitTest.java
@@ -8,6 +8,7 @@ public
 class LimitTest
 {
 static private final double EPSILON = 0.00000001;
+static private final double EPSILON_TO_COMPARE = 0.00001;
 
 /**
  * Testing simple math limits.
@@ -18,11 +19,54 @@ void testSimpleMathLimits()
 {
 	try {
 		assertEquals("Simple math limit №1", -3,
-								 Limit.calculate("(2 * (x ^ 2) - 3 * x - 5) / (x + 1)", "x", 1), EPSILON);
-		assertEquals("Simple math limit №2", Double.NEGATIVE_INFINITY,
-								 Limit.calculate("1 - x", "x", Double.POSITIVE_INFINITY), EPSILON);
-		assertEquals("Simple math limit №3", 0.54030230, Limit.calculate("cos[var]", "var", 1),
+								 Limit.calculate("(2 * (x ^ 2) - 3 * x - 5) / (x + 1)", "x", 1, EPSILON_TO_COMPARE),
 								 EPSILON);
+		assertEquals("Simple math limit №2", Double.NEGATIVE_INFINITY,
+								 Limit.calculate("1 - x", "x", Double.POSITIVE_INFINITY, EPSILON_TO_COMPARE),
+								 EPSILON);
+		assertEquals("Simple math limit №3", 0.54030230,
+								 Limit.calculate("cos[var]", "var", 1, EPSILON_TO_COMPARE), EPSILON);
+		assertEquals("Simple math limit №4", -6.99999988, Limit
+				.calculate("(2 * (x ^ 2) - 3 * x - 5) / (x + 1)", "x", -1, EPSILON_TO_COMPARE), EPSILON);
+	} catch(Exception e) {
+		fail("Handled exception: " + e.toString());
+	}
+}
+
+/**
+ * Testing hard math limits.
+ */
+@Test
+public
+void testHardMathLimits()
+{
+	try {
+		assertEquals("Hard math limit №1", -0.3, Limit
+										 .calculate("(sqrt[x + 6] - sqrt[10 * x - 21]) / (5 * x - 15)", "x", 3, EPSILON_TO_COMPARE),
+								 EPSILON);
+		assertEquals("Hard math limit №2", -12, Limit
+				.calculate("(x ^ 2 + x - 2) / (sqrt[x + 6] - 2)", "x", -2, EPSILON_TO_COMPARE), EPSILON);
+		assertEquals("Hard math limit №3", -0.6666, Limit
+				.calculate("(sqrt[(x / 4), 3] - 0.5) / (sqrt[0.5 + x] - sqrt[2 * x])", "x", 0.5,
+									 EPSILON_TO_COMPARE), 0.0001);
+		assertEquals("Hard math limit №4", 1.25, Limit
+										 .calculate("(e ^ (2 * x) - e ^ (-3 * x)) / (sin[4 * x])", "x", 0, EPSILON_TO_COMPARE),
+								 EPSILON);
+		assertEquals("Hard math limit №5", -0.49999998, // 0.5
+								 Limit.calculate("ln[1 - sin[x]] / (2 * x)", "x", 0, EPSILON_TO_COMPARE), EPSILON);
+		assertEquals("Hard math limit №6", 0, Limit
+				.calculate("(sqrt[8 + (3 * x) + (x ^ 2), 3] - 2) / sqrt[(x ^ 2) + (x ^ 3), 3]", "x", 0,
+									 EPSILON_TO_COMPARE), 0.001 /* double precision */);
+
+		/* todo:
+		assertEquals("Hard math limit №", 0, // 0.5
+								 Limit.calculate("cos[2 * x] ^ (1 / (ln[1 + sin[x] ^ 1]))", "x", 0,
+																 EPSILON_TO_COMPARE), EPSILON);*/
+
+		/* todo:
+				assertEquals("Hard math limit №", 0, Limit
+						.calculate("sqrt[x ^ 3 - 3 * (x ^ 2), 3] - x", "x", Double.POSITIVE_INFINITY,
+											 EPSILON_TO_COMPARE), EPSILON); */
 	} catch(Exception e) {
 		fail("Handled exception: " + e.toString());
 	}
