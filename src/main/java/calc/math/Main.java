241461bf9310314330d38a7941fa3dd959cbdfb0
Sergey F <chukak12@gmail.com>
2021-01-07 21:56:02 +0300

    JavaFX library was added.

--- src/main/java/calc/math/Main.java
+++ src/main/java/calc/math/Main.java
@@ -2,20 +2,36 @@ package calc.math;
 
 import calc.math.expression.Expression;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 public
-class Main
+class Main extends Application
 {
+@Override
+public
+void start(Stage stage) throws Exception
+{
+	stage.setTitle("Engineering calculator");
+	stage.show();
+}
+
 public static
 void main(String[] args)
 {
-	System.out.println("Math expression:");
-	Scanner cin = new Scanner(System.in);
-	try {
-		System.out.println("Result: " + Expression.calculate(cin.nextLine()));
-	} catch(Exception e) {
-		System.out.println("Handled exception: " + e.toString());
+	if(Arrays.asList(args).contains("--non-interactive")) {
+		System.out.println("Math expression:");
+		Scanner cin = new Scanner(System.in);
+		try {
+			System.out.println("Result: " + Expression.calculate(cin.nextLine()));
+		} catch(Exception e) {
+			System.out.println("Handled exception: " + e.toString());
+		}
+	} else {
+		Application.launch();
 	}
 }
 }
